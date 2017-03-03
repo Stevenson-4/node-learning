4e0b519a36bc0151c5a8733d4d4a910ecdfe1cbd
Stevenson Nelli <son.steven4@gmail.com>
2017-03-03 18:03:05 +0530

    Added logging for API

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+/**
+ * https://expressjs.com/en/4x/api.html
+ */
 var express = require('express');
-var config = require('./package.json').config;
+
+/**
+ * https://github.com/expressjs/morgan
+ */
+var morgan = require('morgan');
+
 var app = express();
+
+var config = require('./package.json').config;
 var c = function() {
     console.log.apply(this, arguments);
 };
 var d = function() {
     console.dir.apply(this, arguments);
 }
+
+// Attaching morgan as middleware to the app
+app.use(morgan('tiny'));
 
 app.get('/', function(req,res){
 	return res.send('Stevenson');
